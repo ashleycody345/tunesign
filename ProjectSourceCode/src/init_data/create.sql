2bb74e96e9789e91bace27197189d513fdc01f6d
Ashley Cody <ashley.cody@colorado.edu>
2024-04-02 11:05:02 -0600

    created initial test db tables

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,4 +1,26 @@
+CREATE TABLE zodiacs
+(
+    zodiac VARCHAR(20) PRIMARY KEY,
+    description TEXT
+);
+
 CREATE TABLE users
 (
-    userID int
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    zodiac VARCHAR(20) REFERENCES zodiacs(zodiac)
+);
+
+CREATE TABLE genres
+(
+    genreName VARCHAR(50) PRIMARY KEY,
+    zodiac VARCHAR(20) REFERENCES zodiacs(zodiac)
+);
+
+CREATE TABLE users_to_genres
+(
+    username VARCHAR(50) REFERENCES users(username),
+    genreName VARCHAR(50) REFERENCES genres(genreName),
+    usergenrescore int NOT NULL,
+    PRIMARY KEY (username, genreName)
 );

8e32019927a21ac23503a562a6297123b616a890
Ashley Cody <ashley.cody@colorado.edu>
2024-04-02 10:38:41 -0600

    sample endpoints made for db, api, login requests

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -1,10 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// sample endpoints for default behavior (use this for login procedure for now)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/login');
 })
+
+app.get('/login', (req, res) => {
+  
+})
+
+app.post('/login', (req, res) => {
+  
+})
+
+// sample endpoints for db testing
+
+app.get('/dbselect', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.post('/dbinsert', (req, res) => {
+  res.send('Hello World!');
+})
+
+// sample endpoints for web service implementation (probably will rename and repurpose later?)
+
+app.get('/apirequest', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.post('/apipost', (req, res) => {
+  res.send('Hello World!');
+})
+
+
+
+// open on port 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
